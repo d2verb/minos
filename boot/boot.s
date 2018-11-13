622558cb55686df9f161e2d506c49618eecd0c14
d2verb <licprog73@gmail.com>
2018-11-13 23:14:22 +0900

    Add early page table.

--- boot/boot.s
+++ boot/boot.s
@@ -4,17 +4,48 @@ global start
 extern main
 
 ; constants
-STACK_SIZE equ 4096
+STACK_SIZE    equ 4096
+KERN_BASE     equ 0xc0000000
 
-; .text section
-section .text
+; lower .text section
+section .multiboot.text
 start:
+  ; set page table address
+  mov ecx, page_table
+  mov cr3, ecx
+
+  ; enable 4MB page
+  mov ecx, cr4
+  or ecx, 0x00000010
+  mov cr4, ecx
+
+  ; enable paging
+  mov ecx, cr0
+  or ecx, 0x80000000
+  mov cr0, ecx
+
+  jmp hi_start
+
+section .multiboot.data
+align 4096
+page_table:
+  dd 0x00000083
+  times ((KERN_BASE >> 22) - 1) dd 0
+%assign i 0
+%rep 1024 - (KERN_BASE >> 22) - 1
+  dd 0x00000083 | i << 22
+%assign i i+1
+%endrep
+
+; higher .text section
+section .text
+hi_start:
   ; setup stack
   mov esp, stack_top
-
   ; call c function
   call main
 
+  cli
 loop:
   hlt
   jmp loop
--- kern/kern.c
+++ kern/kern.c
@@ -6,9 +6,12 @@ void main(void) {
   const char *message = "Starting minos!";
   printk("%s\n", message);
 
+  printk("main @ %x\n", (unsigned int)main);
+
   idt_init();
   asm volatile("int $2");
   asm volatile("int $3");
   cli();
+
   return;
 }
